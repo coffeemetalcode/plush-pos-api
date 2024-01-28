d5a23bf394bf8beb3a45e076658ea9ec3616017a
dbstocker <coffeemetalcode@gmail.com>
2024-01-28 08:30:35 -0500

    git clean npm script

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,37 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as CONSTANTS from './shared/constants';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* const DB_MYSQL_CONFIG: TypeOrmModuleOptions = {
+  type: 'mysql',
+  database: 'auto_valuator_instance',
+  entities: [],
+  synchronize: true,
+  host: '127.0.0.1', // <-- changed from 'localhost'
+  port: 3306,
+  username: 'avapp',
+  password: 'Test123$',
+}; */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const DB_SQLITE_CONFIG: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: CONSTANTS.SQLITE_DB_NAME,
+  entities: [],
+  synchronize: true,
+};
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development.local',
     }),
+    TypeOrmModule.forRoot(DB_SQLITE_CONFIG),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/shared/constants.ts
@@ -0,0 +1,2 @@
+// better way to coerce strings?
+export const SQLITE_DB_NAME = `${process.env.SQLITE_DB_NAME}`;
